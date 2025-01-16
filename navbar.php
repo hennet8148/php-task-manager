63fc736229cdb530e7378f438c943719481432d9
hennet8148 <dba.hennet@gmail.com>
2025-01-15 19:46:06 -0500

    nav

---
+++ navbar.php
@@ -0,0 +1,7 @@
+<nav>
+    <a href="neck_form.php">Neck Form</a>
+    <a href="task_form.php">Task Form</a>
+    <a href="task_template_form.php">Task Template</a>
+    <a href="setup_details_form.php">Setup Details</a>
+    <a href="operations_details_form.php">Operations Details</a>
+</nav>
