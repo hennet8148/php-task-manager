6863a4a76ad486ee2a4d15fbb595b7ce027df6d0
Chuck Hennet <dba.hennet@gmail.com>
2025-01-28 13:09:00 -0500

    e

--- track_remodel_task.php
+++ track_remodel_task.php
@@ -16,7 +16,7 @@ if ($conn->connect_error) {
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['action']) && $_POST['action'] === 'add') {
     $taskName = $conn->real_escape_string($_POST['task_name']);
     $timeEstimate = intval($_POST['time_estimate']);
-    $1
+    $categoryId = intval($_POST['category_id']);
     $categoryExists = $conn->query("SELECT 1 FROM Remodel_Categories WHERE CategoryID = $categoryId");
     if ($categoryExists->num_rows === 0) {
         die("Error: Invalid CategoryID. Please select a valid category.");
